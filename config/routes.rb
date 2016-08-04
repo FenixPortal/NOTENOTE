5fb8942edd4d61bfa86d5231f2597a2290883ab5
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-04 13:43:53 -0300

    Change root if user_signed_in?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :notes
+
+  authenticated :user do
+    root to: 'notes#index', as: "authenticated_root"
+  end
+
+  get 'welcome/index'
   root 'welcome#index'
 
-  resources :notes
 end
